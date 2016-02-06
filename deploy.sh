a86063581dd3075635856824946230abdf5240cc
Will <willhy@gmail.com>
2016-02-06 08:56:16 -0600

    Re-arrange. Better scripted deployment.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rsync --delete -vr --delete-excluded \
+	--include="www" --include="www/*" \
+	--exclude="*" \
+	 . will@garfieldunlimited.com:/srv/nginx/garfieldunlimited.com/
+
+scp garfieldunlimited.com.conf will@garfieldunlimited.com:/tmp/
+
+ssh -t will@willhaley.com "sudo mv /tmp/garfieldunlimited.com.conf /etc/nginx/sites-available/ && sudo service nginx restart"
+
